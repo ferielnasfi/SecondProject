124dad264c841f24d1754c6b7c335d17e6247eb4
gscomputer <gscomputer@gscomputer-PC>
2013-02-06 21:24:59 +0100

    my second commit
    Signed-off-by: gscomputer <gscomputer@gscomputer-PC>

---
+++ edu.esprit.piDevProject/src/edu/esprit/piDevProject/first/MyFirstClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.esprit.piDevProject.first;
+
+/**
+ * @author gscomputer
+ *
+ */
+public class MyFirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
